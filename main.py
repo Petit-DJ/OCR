01ecd9f70b73f9ded8e49e7821447f9729545f8f
Petit-DJ <itssimplydjsuryansh@gmail.com>
2026-07-15 14:38:23 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Source - https://stackoverflow.com/q/57888499
+# Posted by singrium
+# Retrieved 2026-07-15, License - CC BY-SA 4.0
+
+import pytesseract
+pytesseract.get_tesseract_version
+#<function get_tesseract_version at 0x7f4b9edd4598>
+print(pytesseract.get_tesseract_version)
+#<function get_tesseract_version at 0x7f4b9edd4598>
